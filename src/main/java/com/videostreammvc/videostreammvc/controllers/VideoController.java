5d5e3bc67dad3bfb77a3355db1f81b25b7c84462
sameerkhanmd112@gmail.com <GitHubSk@112>
2020-04-15 22:43:34 +0530

    mofidified image read by ImageWriteParam, ImageWriter, imageOutpuptStream

--- src/main/java/com/videostreammvc/videostreammvc/controllers/VideoController.java
+++ src/main/java/com/videostreammvc/videostreammvc/controllers/VideoController.java
@@ -1,14 +1,17 @@
 package com.videostreammvc.videostreammvc.controllers;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.Iterator;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
 
 import org.jcodec.api.FrameGrab;
 import org.jcodec.api.JCodecException;
@@ -24,6 +27,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
 public class VideoController {
 
@@ -31,6 +35,7 @@ public class VideoController {
 
     @GetMapping("/videos/{name}")
     public ResponseEntity<UrlResource> getFullVideo(@PathVariable String name) throws MalformedURLException {
+
         UrlResource video = new UrlResource("file:" + path + "/videos/" + name);
 
         return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
@@ -44,14 +49,33 @@ public class VideoController {
         Picture picture = FrameGrab.getFrameFromFile(new File(path + "/videos/" + name), frameNumber);
 
         BufferedImage bufferedImage = AWTUtil.toBufferedImage(picture);
-        ImageIO.write(bufferedImage, "png", new File(path+"/photos/frame.png"));
+        // ImageIO.write(bufferedImage, "png", new File(path+"/photos/frame.png"));
+
+        Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("png");
+
+        ImageWriter writer = writers.next();
+
+        ImageWriteParam param = writer.getDefaultWriteParam();
+        param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        param.setCompressionQuality(0.21f);
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageOutputStream imageOutputStream = ImageIO.createImageOutputStream(byteArrayOutputStream);
+        writer.setOutput(imageOutputStream);
+
+        writer.write(null, new IIOImage( bufferedImage,null, null), param);
+        writer.dispose();
+
+        // System.out.println(bufferedImage.getWidth());
+        // System.out.println(bufferedImage.getHeight());
+
+        // picture.getData();
 
         // InputStream in = getClass().getResourceAsStream("/photos/frame.png");
 
-        Path pathOutput = FileSystems.getDefault().getPath(path+"/photos/frame.png");
+        // Path pathOutput = FileSystems.getDefault().getPath(path+"/photos/frame.png");
 
-        return Files.readAllBytes(pathOutput);
+        // return Files.readAllBytes(pathOutput);
+        return byteArrayOutputStream.toByteArray();
     }
 }
-
-//first-video-streaming-mvc/video-stream-mvc/src/main/resources/static
