37188ec01aea7e3fd286235b8fb4c9a208d163c9
sameerkhanmd112@gmail.com <GitHubSk@112>
2020-04-15 22:44:04 +0530

    Added Thumbnail img tag

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,31 +1,23 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-class App extends React.Component {
-
-	constructor(props) {
-		super(props);
-	}
-
-	componentDidMount() {
-		console.log('mounting app..');
-		
-    }
-    
-    componentWillMount(){
-        console.log("will mount..");
-    }
-
-	render() { 
-		return (
-            <video width="500px" controls>
-                <source src="/videos/Sample1.mp4" type="video/mp4" />
-            </video>
-		)
-	}
+function App(){
+	return (
+		<video width="500px" controls>
+			<source src="/videos/Sample1.mp4" type="video/mp4" />
+		</video>
+	)
 }
 
+function Thumbnail(){
+	return(
+		<img src="/video/Sample1.mp4/image" style={{maxHeight: "100px", maxWidth: "100px"}} ></img>
+	)
+}
 
-
-ReactDOM.render(<App/>, document.getElementById("react"));
-// ReactDOM.render(<Thumbnail />, document.getElementById("thumbnail"));
+ReactDOM.render(
+<div>
+	<Thumbnail />
+	<App/>
+</div>
+, document.getElementById("thumbnail"));
